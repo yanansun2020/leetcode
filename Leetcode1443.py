ecd5614c072d1c9dc2656aa72f683ad494ed2c42
yanansun2020 <hitynsun@gmail.com>
2020-05-15 10:46:32 -0700

    Minimum Time to Collect All Apples in a Tree

---
+++ Leetcode1443.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+
+def main():
+    sol = Solution()
+    result = sol.numOfMinutes(7, 6, [1,2,3,4,5,6,-1], [0,6,5,4,3,2,1])
+    print(result)
+
+if __name__ == "__main__":
+    main()
