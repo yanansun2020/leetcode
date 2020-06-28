38287dfc9bfcd225b26f0d54acc33400dd3c2a71
yanansun2020 <hitynsun@gmail.com>
2020-06-27 22:20:15 -0700

    1044. Longest Duplicate Substring

--- dp/1044-Longest-Duplicate-Substring.py
+++ dp/1044-Longest-Duplicate-Substring.py
@@ -1,50 +1,59 @@
+from collections import defaultdict
 class Solution:
-
     def longestDupSubstring(self, S: str) -> str:
         if not S:
-            return 0
+            return ''
+        h = 1
+        d = 26
+        q = 1<< 32
         left = 0
         right = len(S)
-        N = len(StopAsyncIteration)
-        while left < right:
+        result = ''
+        while left <= right:
             middle = left + (right-left)//2
-            pattern = S[0:middle]
-            if self.Rabin_Karp(S, pattern, middle, N, 0):
+            # if middle == 9:
+            #     print(middle)
+            find_result, find_pattern = self.helper(S, middle, h, d, q)
+            if find_result == True:
+                result = find_pattern
                 left = middle + 1
-
-        return ""
+            if find_result == False:
+                right = middle -1
+        return result
 
     #判断从某个pattern的起始位置， 到pattern_length下 是否有相同的字符串
-    def Rabin_Karp(self, S, pattern, pattern_length, total_length, pattern_start):
-        pattern_hash_code = self.hash(pattern, pattern_length)
-        str_to_match = S[pattern_start:pattern_start + pattern_length]
-        original_hashCode = self.hash(str_to_match)
-        if pattern_hash_code == original_hashCode:
-            return True
-        for index in range(pattern_start + 1, total_length-pattern_length + 1):
-            str_to_match_hashCode = self.getRollingHashCode(original_hashCode, str_to_match, pattern_length, S[index:index+pattern_length])
-            if str_to_match_hashCode == original_hashCode:
-                return True
-            str_to_match = S[index:index+pattern_length]
-            original_hashCode = str_to_match_hashCode
-
-        return False
-
-    def getRollingHashCode(self, original_hashCode, pattern, pattern_length, str_to_match):
-        return (original_hashCode - (pattern[0] * pow(10, pattern_length-1))) * 10 + ord(str_to_match[-1])
-
-    def hash(self, pattern, N):
-        q = (1<<31) - 1
-        hash_code = 0
-        for index in range(N):
-            hash_code += (ord(pattern[index]) * pow(10, N - index -1))
-        return hash_code % q
-
-
+    def helper(self, S, L, h, d, q):
+        #slide window to get String
+        memo = defaultdict(list)
+        for i in range(L-1):
+            h = (h*d)%q
+        hashcode_s = 0
+        for index in range(0, len(S) - L + 1):
+            if index == 0:
+                hashcode_s = self.hashCode(S[index:index+L], L, d, q)
+            else:
+                hashcode_s = (d*(hashcode_s-ord(S[index-1])*h) + ord(S[index+L-1]))%q
+                # if L== 11 or L ==9 or L ==10 or word == "dccdbcebdda":
+                #     print(11111)
+                #     print(hashcode_s)
+                #     print(word)
+                # if hashcode_s == "577004641":
+                #     print(memo[hashcode_s])
+                # 说明之前计算除来过这个hsahcode_s
+                for previous_index in memo[hashcode_s]:
+                    if  S[previous_index:previous_index+L] == S[index:index+L]:
+                        return True, S[index:index+L]
+            memo[hashcode_s].append(index)
+        return False, ''
+    def hashCode(self, pattern, L, d, q):
+        p = 0
+        for i in range(L):
+            p = (d*p + ord(pattern[i]))%q
+        return p
 
 def main():
     sol = Solution()
-    result = sol.findCheapestPrice(3, [[0,1,100],[1,2,100],[0,2,500]], 0, 2, 0)
+    result = sol.longestDupSubstring("banana")
     print(result)
 
 if __name__ == "__main__":
