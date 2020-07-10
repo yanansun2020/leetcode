cac1494caf077b470eefc52b46526a1d0687eaec
yanansun2020 <hitynsun@gmail.com>
2020-07-09 21:11:55 -0700

    221. Maximal Square

---
+++ dp/221-Maximal-Square.py
@@ -0,0 +1,26 @@
+from typing import List
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        rows = len(matrix)
+        cols = len(matrix[0])
+        dp = [[0 for i in range(cols) ] for j in range(rows)]
+        result = 0
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                result = max(result, dp[i][j])
+        return result * result
+
+def main():
+    sol = Solution()
+    result = sol.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]])
+    print(result)
+
+if __name__ == "__main__":
+    main()
