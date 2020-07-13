4d95da7b3764a9ad06614d236f46b8f8c80f402d
yanansun2020 <hitynsun@gmail.com>
2020-07-13 14:27:36 -0700

    430. Flatten a Multilevel Doubly Linked List

--- list/430-Flatten-a-Multilevel-Doubly-Linked-List.py
+++ list/430-Flatten-a-Multilevel-Doubly-Linked-List.py
@@ -8,6 +8,26 @@ class Node:
         self.child = child
 
 class Solution:
+    #dfs+stack:
+    def flatten_stack(self, head: 'Node') -> 'Node':
+        if not head: return head
+        stack, order = [head], []
+
+        while stack:
+            last = stack.pop()
+            order.append(last)
+            if last.next:
+                stack.append(last.next)
+            if last.child:
+                stack.append(last.child)
+
+        for i in range(len(order) - 1):
+            order[i+1].prev = order[i]
+            order[i].next = order[i+1]
+            order[i].child = None
+
+        return order[0]
+    #dfs _ recursive
     def flatten(self, head: 'Node') -> 'Node':
         if not head:
             return None
