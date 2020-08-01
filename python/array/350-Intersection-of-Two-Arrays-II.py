bdfb69ddda895d18c35374529ac4a6f8b73af2de
yanansun2020 <hitynsun@gmail.com>
2020-07-31 23:51:47 -0700

    350 optimize

--- python/array/350-Intersection-of-Two-Arrays-II.py
+++ python/array/350-Intersection-of-Two-Arrays-II.py
@@ -3,25 +3,20 @@ from collections import defaultdict
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         memo_num1 = defaultdict(int)
-        memo_num2 = defaultdict(int)
+        ans = []
         for num in nums1:
             memo_num1[num] += 1
         for num in nums2:
-            memo_num2[num] += 1
-        ans = []
-        for key, value in memo_num1.items():
-            if key in memo_num2:
-                count = min(value, memo_num2[key])
-                while count > 0:
-                    ans.append(key)
-                    count -= 1
+            if num in memo_num1 and memo_num1[num]>0:
+                ans.append(num)
+                memo_num1[num] -= 1
         return ans
 
 def main():
     sol = Solution()
     result = sol.intersect([1,2,2,1], [2,2])
     print(result)
-    result = sol.intersect([4,9,5], [2,2])
+    result = sol.intersect([4,9,5], [9,4,9,8,4])
     print(result)
 
 if __name__ == "__main__":
