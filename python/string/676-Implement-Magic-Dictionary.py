934d5138741068c4f89b8715a93a7bf1e9c96a45
yanansun2020 <hitynsun@gmail.com>
2020-08-05 17:28:39 -0700

    676-magic dictionary

--- python/string/676-Implement-Magic-Dictionary.py
+++ python/string/676-Implement-Magic-Dictionary.py
@@ -4,6 +4,8 @@
 # param_2 = obj.search(word)
 from collections import defaultdict
 from typing import List
+# Solution1 Trie
+# Solution2-HashMap - {.ello-h; h.llo-e; he.lo-l; hel.o-l; hell.-o}
 class Trie:
     def __init__(self):
         self.trieMap = defaultdict(Trie)
@@ -22,15 +24,10 @@ class Trie:
 class MagicDictionary:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = Trie()
+        self.letters = "abcdefghijklmnopqrstuvwxyz"
 
     def buildDict(self, dict: List[str]) -> None:
-        """
-        Build a dictionary through a list of words
-        """
         for word in dict:
             trie = self.root
             for ch in word:
@@ -42,28 +39,44 @@ class MagicDictionary:
             trie.isEnd = True
 
     def search(self, word: str) -> bool:
-        """
-        Returns if there is any word in the trie that equals to the given word after modifying exactly one character
-        """
         trie = self.root
-        def dfs(index, trie, has_changed):
-            if index == len(word):
-                return trie.isEnd and has_changed
-            ch = word[index]
+        def dfs(index, trie, word_):
+            if index == len(word_):
+                return trie.isEnd
+            ch = word_[index]
             node = trie.getNodeByCh(ch)
-            if node:
-                return dfs(index + 1, node, has_changed)
-            else:
-                if has_changed:
-                    return False
-                else:
-                    ch_candidates = trie.getMapKeys()
-                    for ch_candidate in ch_candidates:
-                        result = dfs(index+1, trie.getNodeByCh(ch_candidate), True)
-                        if result:
-                            return True
-                    return False
-        return dfs(0, trie, False)
+            if not node:
+                return False
+            return dfs(index + 1, node, word_)
+
+        for index in range(len(word)):
+            for s in self.letters:
+                if s == word[index]:
+                    continue
+                word_ = word[0:index] + s + word[index+1:]
+                if dfs(0, trie, word_):
+                    return True
+        return False
+
+
+    # def dfs(index, trie, has_changed):
+    #         if index == len(word):
+    #             return trie.isEnd and has_changed
+    #         ch = word[index]
+    #         node = trie.getNodeByCh(ch)
+    #         if node:
+    #             return dfs(index + 1, node, has_changed)
+    #         else:
+    #             if has_changed:
+    #                 return False
+    #             else:
+    #                 ch_candidates = trie.getMapKeys()
+    #                 for ch_candidate in ch_candidates:
+    #                     result = dfs(index+1, trie.getNodeByCh(ch_candidate), True)
+    #                     if result:
+    #                         return True
+    #                 return False
+    #     return dfs(0, trie, False)
 
 def main():
     sol = MagicDictionary()
