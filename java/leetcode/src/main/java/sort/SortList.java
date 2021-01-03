aafc5ae41f74cb33c1669cbb9f72871d0dbed932
sunyanan <hitynsun@gmail.com>
2021-01-03 00:11:05 -0800

    sort a linkedlist by insert sort and merge list

--- java/leetcode/src/main/java/sort/SortList.java
+++ java/leetcode/src/main/java/sort/SortList.java
@@ -4,7 +4,61 @@ import list.ListNode;
 import org.junit.Test;
 
 public class SortList {
-    public ListNode sortList(ListNode head) {
+    /**
+     * merge sort linkedlist
+     * @param head
+     * @return
+     */
+    public ListNode sortList(ListNode head){
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+    ListNode merge(ListNode left, ListNode right){
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        while (left != null && right != null){
+            if (left.val < right.val){
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        if (left != null){
+            cur.next = left;
+        }
+        if (right != null){
+            cur.next = right;
+        }
+        return head.next;
+    }
+
+    private ListNode findMid(ListNode node){
+        ListNode fast = node;
+        ListNode slow = node;
+        ListNode pre = null;
+        while (fast != null && fast.next != null){
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        return slow;
+    }
+
+    /**
+     * insert sort linkedlist
+     * @param head
+     * @return
+     */
+    public ListNode sortList_TLE(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
