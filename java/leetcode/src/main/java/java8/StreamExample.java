f17186cb4e6aba4f835539ef2efaded915e34f78
yanansun2020 <hitynsun@gmail.com>
2021-06-03 17:21:25 -0700

    add stream examples

--- java/leetcode/src/main/java/java8/StreamExample.java
+++ java/leetcode/src/main/java/java8/StreamExample.java
@@ -1,5 +1,16 @@
 package java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//stream working with collection
 public class StreamExample {
-    
+    public static void main(String[] args) {
+        List<Person> people = Arrays.asList(new Person("Yanan", "Sun", 1), new Person("Pa", "Pu", 2), new Person("C", "C", 1));
+        // people.stream().forEach(p -> System.out.println(p.getFirstName()));
+        people.stream().filter(p->p.getLastName().startsWith("C")).forEach(p -> System.out.println(p.getFirstName()));
+        List<Person> people1 = people.stream().filter(p->p.getLastName().startsWith("C")).collect(Collectors.toList());
+        people.parallelStream().filter(p->p.getLastName().startsWith("C")).collect(Collectors.toList());
+    }
 }
