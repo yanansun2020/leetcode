50be88aaa18e0018c74b6c791ec25ea74c1aa1f5
yanansun2020 <hitynsun@gmail.com>
2022-08-31 10:52:32 -0700

    add pass tsting

--- java/leetcode/src/main/java/cisco/PassphraseEncryptor.java
+++ java/leetcode/src/main/java/cisco/PassphraseEncryptor.java
@@ -13,10 +13,12 @@
 package cisco;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
@@ -29,6 +31,7 @@ public class PassphraseEncryptor {
     private static final int iterations = 10 * 1024;
     private static final int saltLen = 32;
     private static final int desiredKeyLen = 256;
+    private static String FIPS_FLAG = System.getenv("FIPS_ENABLED") == null ? "true" : System.getenv("FIPS_ENABLED");
 
     /**
      * Computes a salted PBKDF2 hash of given plaintext passphrase suitable for
@@ -62,21 +65,38 @@ public class PassphraseEncryptor {
     // using PBKDF2 from Sun, an alternative is https://github.com/wg/scrypt
     // cf. http://www.unlimitednovelty.com/2012/03/dont-use-bcrypt.html
     private static String hash(String passphrase, byte[] salt) throws IllegalArgumentException, InvalidKeySpecException,
-            NoSuchAlgorithmException {
+        NoSuchAlgorithmException {
         if (passphrase == null || passphrase.length() == 0) {
-            throw new IllegalArgumentException("Empty passphrases are not supported.");
+            throw new IllegalArgumentException("Empty password are not supported.");
+        }
+        if (!StringUtils.isEmpty(FIPS_FLAG) && Boolean.valueOf(FIPS_FLAG)) {
+            return hash_sha256(passphrase, salt);
         }
         SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
         SecretKey key = f.generateSecret(new PBEKeySpec(passphrase.toCharArray(), salt, iterations, desiredKeyLen));
-
-        // key.get
         return Base64.encodeBase64String(key.getEncoded());
 
     }
 
+    private static String hash_sha256(String password, byte[] salt) {
+        String generatedPassword = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt);
+            byte[] bytes = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytes.length; i++) {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+        }
+        return generatedPassword;
+    }
+
     public static void main(String[] args) {
         try {
-            String s = getSaltedHash("Maglev12");
+            String s = getSaltedHash("password");
             System.out.println(s);
         } catch (InvalidKeySpecException e) {
             e.printStackTrace();
